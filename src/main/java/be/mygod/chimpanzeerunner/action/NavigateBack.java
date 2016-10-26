1fd56bca78271557c115f56e42ed1e3091485d5b
Mygod <contact-git@mygod.be>
2016-10-26 23:13:06 +0800

    Support whitelisting apps

--- src/main/java/be/mygod/chimpanzeerunner/action/NavigateBack.java
+++ src/main/java/be/mygod/chimpanzeerunner/action/NavigateBack.java
@@ -4,17 +4,36 @@ import be.mygod.chimpanzeerunner.test.TestAbortException;
 import be.mygod.chimpanzeerunner.test.TestManager;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.WeakHashMap;
 import java.util.stream.Stream;
 
 public class NavigateBack extends AbstractAction {
+    private static final HashSet<URI> URI_WHITE_LIST;
     private static final WeakHashMap<TestManager, URI> initialLocation = new WeakHashMap<>();
+
+    static {
+        HashSet<URI> list;
+        try {
+            list = new HashSet<>(Arrays.asList(
+                    new URI("android://com.android.packageinstaller/.permission.ui.GrantPermissionsActivity"),
+                    new URI("android://com.android.settings/.Settings$AppWriteSettingsActivity")
+            ));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            list = new HashSet<>();
+        }
+        URI_WHITE_LIST = list;
+    }
+
     public static Stream<AbstractAction> getActions(TestManager manager) {
         URI location = null;
         int i;
         for (i = 10; i > 0; --i) {
             location = manager.getLocation();
-            if (manager.getPackageName().equals(location.getHost())) break; else { // TODO: whitelisting some system app?
+            if (manager.getPackageName().equals(location.getHost()) || URI_WHITE_LIST.contains(location)) break; else {
                 System.out.printf("Unexpected location: %s. Pressing back in hope of returning to original package...\n", location);
                 manager.navigateBack();
             }
